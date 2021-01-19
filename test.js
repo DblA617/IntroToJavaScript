e586512423ac09ead55bebc32494f651faf402c5
Aarika Turner <turneraarika@gmail.com>
2021-01-18 21:20:32 -0500

    push

---
+++ test.js
@@ -0,0 +1,10 @@
+// the first alert
+alert("Hello, World");
+
+//the second alert
+alert("Hello, World");
+
+// make the image invisile
+// make the image move from top to bottom
+// make the image visible
+ 
